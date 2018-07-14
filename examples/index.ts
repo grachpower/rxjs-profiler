66ff51c899d84313d758dbcb11249003b8fcbca4
Artyom Andreev <amiscary@gmail.com>
2018-07-15 01:07:38 +0300

    add packages

---
+++ examples/index.ts
@@ -0,0 +1,16 @@
+import { Subject } from "rxjs";
+import { tap } from "rxjs/operators";
+
+const stream$ = new Subject();
+
+stream$
+    .pipe(
+        tap(value => console.log(value)),
+    )
+    .subscribe();
+
+stream$.next('lol');
+stream$.next('kek');
+stream$.next('cheburek');
+
+stream$.unsubscribe();
