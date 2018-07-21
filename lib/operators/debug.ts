ea6f3f48f939f82371805db1284c9738b68c2fc3
Artyom Andreev <amiscary@gmail.com>
2018-07-21 22:10:59 +0300

    remove redundant code

--- lib/operators/debug.ts
+++ lib/operators/debug.ts
@@ -1,4 +1,4 @@
-import { Observable, Operator, Subscriber, TeardownLogic } from "rxjs/index";
+import { Observable } from "rxjs/index";
 
 export function debug<T>(name: string) {
     return function debugOperatorFunction(source: Observable<T>): Observable<T> {
@@ -6,40 +6,4 @@ export function debug<T>(name: string) {
 
         return source;
     };
-}
-
-class DebugOperator<T> implements Operator<T, T> {
-    constructor(private debugName: string) { }
-
-    call(subscriber: Subscriber<T>, source: any): TeardownLogic {
-        return source.subscribe(new DebugSubscriber(subscriber, this.debugName));
-    }
-}
-
-/**
- * We need this JSDoc comment for affecting ESDoc.
- * @ignore
- * @extends {Ignored}
- */
-class DebugSubscriber<T> extends Subscriber<T> {
-
-    constructor(destination: Subscriber<T>,
-                private debugName: string) {
-        super(destination);
-    }
-
-    // the try catch block below is left specifically for
-    // optimization and perf reasons. a tryCatcher is not necessary here.
-    // protected _next(value: T) {
-    //     let result: any;
-    //     try {
-    //         result = this.predicate.call(this.thisArg, value, this.count++);
-    //     } catch (err) {
-    //         this.destination.error(err);
-    //         return;
-    //     }
-    //     if (result) {
-    //         this.destination.next(value);
-    //     }
-    // }
 }
